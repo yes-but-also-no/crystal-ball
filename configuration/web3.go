00bf3182dcf3d18577836994725d12a2ece85476
Rirush <RaccoonRirush@gmail.com>
2021-05-03 11:06:01 +0800

    fix: add orakuru_core field to web3 configuration

--- configuration/web3.go
+++ configuration/web3.go
@@ -5,5 +5,6 @@ import "crypto/ecdsa"
 type Web3 struct {
 	URL           string            `yaml:"url"`
 	RawPrivateKey string            `yaml:"private_key"`
+	OrakuruCore   string            `yaml:"orakuru_core"`
 	PrivateKey    *ecdsa.PrivateKey `yaml:"-"`
 }
